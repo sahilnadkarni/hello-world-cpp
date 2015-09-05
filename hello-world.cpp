a0b7ccbfe4a37ac4da2eef68cf6800c001a15fb6
Sahil Nadkarni <sahil.nadkarni29@gmail.com>
2015-09-04 20:44:20 -0400

    Initial Checkin

---
+++ hello-world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "'Sup Swag Sahil!" << endl;
+    cout << " You are more swag than anyone!" << endl;
+    cout << " Welcome to your Dell Inspiron Mini!" << endl;
+    return 0;
+}
